a06f20ef0e44f175f33bd7cbd25f763f9d8d67e7
过宇 <guoyu@thtf.com.cn>
2019-10-17 17:30:01 +0800

    none

--- showRunDat.py
+++ showRunDat.py
@@ -9,21 +9,25 @@ import getRunDat
 from pyecharts.charts import Line,Page
 from pyecharts import options as opts
 
-def showDat():
+from pyecharts.faker import Collector, Faker
+
+C = Collector()
+listDate = []
+listDistance = []
+listTime = []
+listPace = []
+listCal = []
+listHeartRate = []
+listStepFrequency = []
+listStride = []
+countMon = []
+DistanceMon = []
+countYear = []
+DistanceYear = []
+
+def datPrepare():
     rawDat = getRunDat.getRunDat()
     height = rawDat.shape[0]
-    listDate = []
-    listDistance = []
-    listTime = []
-    listPace= []
-    listCal = []
-    listHeartRate = []
-    listStepFrequency = []
-    listStride = []
-    countMon = []
-    DistanceMon = []
-    countYear = []
-    DistanceYear = []
     for row in range(height-1):
         listDate.append(rawDat.iloc[row, 0])
         if( np.isnan(rawDat.iloc[row, 1]) ):
@@ -39,116 +43,146 @@ def showDat():
         DistanceMon.append(rawDat.iloc[row, 9])
         countYear.append(rawDat.iloc[row, 10])
         DistanceYear.append(rawDat.iloc[row, 11])
-    lineDist = Line()
-    lineDist.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
-                name="日期",
-                #splitline_opts=opts.SplitLineOpts(is_show=True),
-                is_scale=True
-            ))
-    lineDist.add_yaxis("跑步距离",listDistance).set_global_opts(
-                        yaxis_opts=opts.AxisOpts(
-                            type_="value",
-                            is_scale=True
-                        ),
-                        title_opts=opts.TitleOpts(title="跑步距离曲线")
+
+@C.funcs
+def showDatDistance() -> Line:
+    C = (
+        Line().add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+                    name="日期",
+                    #splitline_opts=opts.SplitLineOpts(is_show=True),
+                    is_scale=True
+                ))
+                .add_yaxis("跑步距离",listDistance).set_global_opts(
+                            yaxis_opts=opts.AxisOpts(
+                                type_="value",
+                                is_scale=True
+                            ),
+                            title_opts=opts.TitleOpts(title="跑步距离曲线")
+        )
     )
-    lineDist.render("dateDistance.html")
+    #lineDist.render("dateDistance.html")
+    return C
 
-    lineTime = Line()
-    lineTime.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
-                name="日期",
-                #splitline_opts=opts.SplitLineOpts(is_show=True),
-                is_scale=True
-            ))
-    lineTime.add_yaxis("跑步时间",listTime).set_global_opts(
-                        yaxis_opts=opts.AxisOpts(
-                            type_="time",
-                            is_scale=True
-                        ),
-                        title_opts=opts.TitleOpts(title="跑步时间曲线")
+@C.funcs
+def showDatTime() -> Line:
+    C= (
+        Line().add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+                    name="日期",
+                    #splitline_opts=opts.SplitLineOpts(is_show=True),
+                    is_scale=True
+                ))
+                .add_yaxis("跑步时间",listTime).set_global_opts(
+                            yaxis_opts=opts.AxisOpts(
+                                type_="time",
+                                is_scale=True
+                            ),
+                            title_opts=opts.TitleOpts(title="跑步时间曲线")
+        )
     )
+    #lineTime.render("dateTime.html")
+    return C
 
-    lineTime.render("dateTime.html")
-
-    linePace = Line()
-    linePace.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+@C.funcs
+def showDatPace() -> Line:
+    C = (
+        Line()
+            .add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
                 name="日期",
                 #splitline_opts=opts.SplitLineOpts(is_show=True),
                 is_scale=True
             ))
-    linePace.add_yaxis("每公里配速",listPace).set_global_opts(
+            .add_yaxis("每公里配速",listPace).set_global_opts(
                         yaxis_opts=opts.AxisOpts(
                             type_="time",
                             is_scale=True
                         ),
                         title_opts=opts.TitleOpts(title="配速曲线")
+        )
     )
-    linePace.render("paceTime.html")
+    #linePace.render("paceTime.html")
+    return C
 
-    lineCal = Line()
-    lineCal.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+@C.funcs
+def showDatCal() -> Line:
+    C = (
+        Line().add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
                 name="日期",
                 #splitline_opts=opts.SplitLineOpts(is_show=True),
                 is_scale=True
             ))
-    lineCal.add_yaxis("消耗能量",listCal).set_global_opts(
+            .add_yaxis("消耗能量",listCal).set_global_opts(
                         yaxis_opts=opts.AxisOpts(
                             type_="value",
                             is_scale=True,
                         ),
                         title_opts=opts.TitleOpts(title="能耗曲线")
+        )
     )
-    lineCal.render("calConsum.html")
+    #lineCal.render("calConsum.html")
+    return C
 
-    lineHeartRate = Line()
-    lineHeartRate.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+@C.funcs
+def showDatHeartRate() -> Line:
+    C = (
+        Line().add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
                 name="日期",
                 #splitline_opts=opts.SplitLineOpts(is_show=True),
                 is_scale=True
             ))
-    lineHeartRate.add_yaxis("平均心率",listHeartRate).set_global_opts(
+            .add_yaxis("平均心率",listHeartRate).set_global_opts(
                         yaxis_opts=opts.AxisOpts(
                             type_="value",
                             is_scale=True,
                         ),
                         title_opts=opts.TitleOpts(title="心率曲线")
+        )
     )
-    lineHeartRate.render("heartRate.html")
+    #lineHeartRate.render("heartRate.html")
+    return C
 
-    lineStepFrequency = Line()
-    lineStepFrequency.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+@C.funcs
+def showDatFrequency() -> Line:
+    C = (
+        Line().add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
                 name="日期",
                 #splitline_opts=opts.SplitLineOpts(is_show=True),
                 is_scale=True
             ))
-    lineStepFrequency.add_yaxis("平均步频",listStepFrequency).set_global_opts(
+            .add_yaxis("平均步频",listStepFrequency).set_global_opts(
                         yaxis_opts=opts.AxisOpts(
                             type_="value",
                             is_scale=True,
                         ),
                         title_opts=opts.TitleOpts(title="步频曲线")
+        )
     )
-    lineStepFrequency.render("stepFrequency.html")
+    #lineStepFrequency.render("stepFrequency.html")
+    return C
 
-    lineStride = Line()
-    lineStride.add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
+@C.funcs
+def showDatStride() -> Line:
+    C = (
+        Line().add_xaxis(listDate).set_global_opts(xaxis_opts=opts.AxisOpts(
                 name="日期",
                 #splitline_opts=opts.SplitLineOpts(is_show=True),
                 is_scale=True
             ))
-    lineStride.add_yaxis("平均步幅",listStride).set_global_opts(
+            .add_yaxis("平均步幅",listStride).set_global_opts(
                         yaxis_opts=opts.AxisOpts(
                             type_="value",
                             is_scale=True,
                         ),
                         title_opts=opts.TitleOpts(title="步幅曲线")
+        )
     )
-    lineStride.render("stepStride.html")
+    #lineStride.render("stepStride.html")
+    return C
 
 
 if __name__ == '__main__':
     time1 = time.time()
     print(time1)
-    showDat()
+    datPrepare()
+    Page().add(*[fn() for fn, _ in C.charts]).render("runDat.html")
     time2 = time.time()
     print(time2)
